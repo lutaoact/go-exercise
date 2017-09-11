05e5636f44d375be3013edb04452e3dd3f72ed2e
lutaoact <i@lutao.me>
2017-09-11 16:36:14 +0800

    pool

---
+++ poolUse.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"pool"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	halfGoroutines  = 10
+	maxGoroutines   = 20
+	pooledResources = 10
+)
+
+type dbConnection struct {
+	ID int32
+}
+
+func (dbConn *dbConnection) Close() error {
+	log.Println("Close: Connection", dbConn.ID)
+	return nil
+}
+
+var idCouter int32
+
+func createConnection() (io.Closer, error) {
+	id := atomic.AddInt32(&idCouter, 1)
+	log.Println("Create: New Connection", id)
+
+	return &dbConnection{id}, nil
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(maxGoroutines)
+
+	p, err := pool.New(createConnection, pooledResources)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for query := 0; query < halfGoroutines; query++ {
+		go func(q int) {
+			performQueries(q, p)
+			wg.Done()
+		}(query)
+	}
+
+	//延时一会，让之前的任务处理完，资源还到通道中
+	time.Sleep(time.Duration(200) * time.Millisecond)
+
+	for query := halfGoroutines; query < maxGoroutines; query++ {
+		go func(q int) {
+			performQueries(q, p)
+			wg.Done()
+		}(query)
+	}
+	wg.Wait()
+
+	log.Println("Shutdown Program.")
+	p.Close()
+}
+
+func performQueries(query int, p *pool.Pool) {
+	conn, err := p.Acquire()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer p.Release(conn)
+	time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+	log.Printf("QID[%d] CID[%d]\n", query, conn.(*dbConnection).ID)
+}
---
+++ src/pool/pool.go
@@ -0,0 +1,80 @@
+package pool
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+)
+
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ErrPoolClosed = errors.New("Pool has been closed.")
+
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("Size value too small.")
+	}
+	return &Pool{
+		factory:   fn,
+		resources: make(chan io.Closer, size),
+	}, nil
+}
+
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		log.Println("Acquire:", "Shared Resource")
+		if !ok {
+			return nil, ErrPoolClosed
+		}
+		return r, nil
+	default:
+		log.Println("Acquire:", "New Resource")
+		return p.factory()
+	}
+}
+
+func (p *Pool) Release(r io.Closer) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	// 试图将这个资源放入队列
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+
+	//如果队列已满，则关闭这个资源
+	default:
+		log.Println("Release:", "Closing")
+		r.Close()
+	}
+}
+
+func (p *Pool) Close() {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if p.closed {
+		return
+	}
+
+	p.closed = true
+
+	//在清空通道里的资源之前，将通道关闭，如果不这样做，会发生死锁？
+	close(p.resources)
+
+	for r := range p.resources {
+		r.Close()
+	}
+}
