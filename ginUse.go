3b755ed6441b165db6238d290639fc09df12d93f
lutaoact <i@lutao.me>
2017-10-11 09:57:47 +0800

    ginUse.go

--- ginUse.go
+++ ginUse.go
@@ -1,10 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		scopes := c.QueryArray("scope") //curl 'http://localhost:8080/ping?scope=1&scope=2'
+		fmt.Printf("scopes = %+v\n", scopes)
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 	r.Run()
