a9a3aa43a16ce7ce688a407a40450933f8e51f8c
lutaoact <i@lutao.me>
2017-10-13 21:35:48 +0800

    ginUse.go

--- ginUse.go
+++ ginUse.go
@@ -4,14 +4,23 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/qbox/stark/hms/api"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		scopes := c.QueryArray("scope") //curl 'http://localhost:8080/ping?scope=1&scope=2'
-		fmt.Printf("scopes = %+v\n", scopes)
-		c.JSON(200, gin.H{"message": "pong"})
+		run2(c)
 	})
 	r.Run()
+}
+
+func run1(c *gin.Context) { //curl 'http://localhost:8080/ping?scope=1&scope=2'
+	scopes := c.QueryArray("scope")
+	fmt.Printf("scopes = %+v\n", scopes)
+	c.JSON(200, gin.H{"message": "pong"})
+}
+
+func run2(c *gin.Context) { //curl 'http://localhost:8080/ping'
+	api.E(c, api.NewHttpErr(401, "unauthorized", nil))
 }
